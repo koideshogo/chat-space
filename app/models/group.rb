009ade9e04b075d39d26db4a67454430ce406243
koideshogo <56552221+koideshogo@users.noreply.github.com>
2019-11-04 11:31:27 +0900

    model_user_association

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,9 +2,4 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :group_users
   validates :name, presence: true, uniqueness: true
-
-
-  has_many :messages
-  hasa_many :group_users
-  has_many :groups, through: :group_users
 end
