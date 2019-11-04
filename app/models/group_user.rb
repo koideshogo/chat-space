6859e57876c378257efa7c0042ef419cbc953599
koideshogo <56552221+koideshogo@users.noreply.github.com>
2019-11-04 11:36:03 +0900

    groups_user_association

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,8 +1,4 @@
 class GroupUser < ApplicationRecord
-devise :database_authenticatable, :registerable,
-       :recoverable, :rememberable,  :validatable
-
-       has_many :messages
-       hasa_many :group_users
-       has_many :groups, through: :group_users
+  belongs_to :group
+  belongs_to :user
 end
