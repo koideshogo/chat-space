2d6499d62520e2e5f7ed905a26ee7cd4ddb48438
koideshogo <56552221+koideshogo@users.noreply.github.com>
2019-12-01 16:48:14 +0900

    自動スクロール修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -63,8 +63,8 @@ $(function() {
         messages.forEach(function (message) {
           insertHTML = buildHTML(message);
           $(".messages").append(insertHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
         })
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       })
 
       .fail(function() {
